b51a9bebcc8ea90f2c815daed210f921d3e16587
Akourne Bursche <Crocodoctopus@gmail.com>
2018-07-08 17:13:14 -0800

    Support for first mapper added to cart

--- src/mappers/mapper.rs
+++ src/mappers/mapper.rs
@@ -1,6 +1,6 @@
 use cartridge::*;
 
 pub trait Mapper {
-    fn get(u16, &Cartridge) -> u8;
-    fn set(u16, u8, &mut Cartridge);
+    fn get(&self, u16, &Cartridge) -> u8;
+    fn set(&mut self, u16, u8, &mut Cartridge);
 }
